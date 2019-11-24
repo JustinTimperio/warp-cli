ae5666d5d74a8c19b158f9b4762d85e8c9e3e53f
Justin Timperio <justintimperio@gmail.com>
2019-11-23 22:48:27 -0500

    Fixing issue with python_scripts

--- core/setup.py
+++ core/setup.py
@@ -8,11 +8,11 @@ from python_scripts import *
 ########
 def build_wdt(base_dir):
     import re
-    ## build and setup wdt dependencies depending on linux distro
+    ### build and setup wdt dependencies depending on linux distro
     os_name = os_distro()
     #############
     if re.search(r'arch', os_name.lower()):
-        aur_tool = input('Enter the install command for your tool... (I.E. "pacaur -S"): ')
+        aur_tool = input('Enter the Install Command for Your Tool... (I.E. "pacaur -S"): ')
         if len(aur_tool) > 2:
             os.system(aur_tool + " wdt-git")
             sys.exit('Done!')
@@ -26,13 +26,13 @@ def build_wdt(base_dir):
         apt('cmake libjemalloc-dev libgoogle-glog-dev libboost-system-dev libdouble-conversion-dev openssl build-essential libboost-all-dev libssl-dev libgtest-dev libevent-dev')
     #############
     else:
-        sys.exit('Automated package installs for ' + os_name + ' are not supported.')
+        sys.exit('Automated Package Installs for ' + os_name + ' Are NOT Supported.')
     
-    ## download and build wdt from github source
-    mkdir(base_dir + '/build', 'u')
+    ### download and build wdt from github source
+    mkdir(base_dir + '/build', sudo=False)
     os.system('cd ' + base_dir + '/build && git clone https://github.com/facebook/folly.git')
     os.system('cd ' + base_dir + '/build/folly && git checkout v2019.09.02.00')
-    ## unused command while folly is broke
+    ### unused command while folly is broke
     #  os.system('cd ' + base_dir + '/build/folly && git checkout "$(git describe --abbrev=0 --always)"')
     os.system('cd ' + base_dir + '/build && git clone https://github.com/facebook/wdt.git')
     mkdir(base_dir + '/build/wdt/_build', 'u')
@@ -42,17 +42,17 @@ def build_wdt(base_dir):
 ## Setup Warp-CLI
 ########
 def setup_warp(base_dir):
-    ## setup warp-cli dirs
-    mkdir(base_dir + '/pool', 'u')
-    mkdir(base_dir + '/macros', 'u')
+    ### setup warp-cli dirs
+    mkdir(base_dir + '/pool', sudo=False)
+    mkdir(base_dir + '/macros', sudo=False)
     os.system('sudo chmod 777 ' + base_dir + '/pool ' + base_dir + '/macros')
-    ## link warp in bash to warp.py
-    rm_file('/usr/bin/warp', 'r')
+    ### link warp in bash to warp.py
+    rm_file('/usr/bin/warp', sudo=True)
     os.system('sudo ln -s ' + base_dir + '/core/warp.py /usr/bin/warp')
-    print('Warp-CLI is now setup and registered in /usr/bin!')
+    print('Warp-CLI is Now Setup and Registered in /usr/bin!')
     
     ### Start Automated Setup
-    build = yn_frame('Do you want to attempt an automatic WDT build and install?')
+    build = yn_frame('Do You Want to Attempt an Automatic WDT Build and Install?')
     if build == True:
         build_wdt(base_dir)
         os.system("echo '=============================' && warp --version && echo '============================='")
@@ -60,7 +60,7 @@ def setup_warp(base_dir):
         print('Refer to https://github.com/facebook/wdt/blob/master/build/BUILD.md for manual builds.')
 
 def setup_warp_remote(ssh_alias, base_dir, dev):
-    ## tunnel to a remote machine and install warp-cli
+    ### tunnel to a remote machine and install warp-cli
     git_clone = ' "cd ' + base_dir + ' && git clone --recurse-submodules https://github.com/JustinTimperio/warp-cli.git &&'
     build = ' python3 ' + base_dir + '/warp-cli/core/warp.py --install"' 
     if dev == False:
@@ -69,6 +69,6 @@ def setup_warp_remote(ssh_alias, base_dir, dev):
         os.system('ssh ' + ssh_alias + git_clone + ' cd warp-cli/ && git checkout development && git submodule update --init --recursive &&' + build)
 
 def uninstall_warp(base_dir):
-    rm_dir(base_dir, 'r')
-    rm_file('/usr/bin/warp', 'r')
+    rm_dir(base_dir, sudo=True)
+    rm_file('/usr/bin/warp', sudo=True)
     print('Warp-CLI Uninstalled!')
